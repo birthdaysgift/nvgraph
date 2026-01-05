fc7119c2c0ea136b227801ed741586718693ba34
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2026-01-06 00:50:07 +0400

    Add simple parser and formatter for parents and refs

--- lua/nvgraph/formatter.lua
+++ lua/nvgraph/formatter.lua
@@ -1,10 +1,22 @@
 M = {}
 
 
+local function format_parents(parents)
+  return " [" .. table.concat(parents, ", ") .. "] "
+end
+
+
+local function format_refs(refs)
+  return " (" .. table.concat(refs, ", ") .. ") "
+end
+
+
 function M.format_lines(graph_data)
   local lines = {}
   for _, data in ipairs(graph_data) do
-    local line = " * " .. data["hash"] .. " [" .. data["parents"] .. "] (" .. data["refs"] .. ") "
+    local parents = format_parents(data["parents"])
+    local refs = format_refs(data["refs"])
+    local line = " * " .. data["hash"] .. parents .. refs
     table.insert(lines, line)
   end
   return lines
--- lua/nvgraph/parser.lua
+++ lua/nvgraph/parser.lua
@@ -4,21 +4,31 @@ local utils = require("nvgraph.utils")
 M = {}
 
 
+local function parse_parents(parents_str)
+  return utils.strsplit(parents_str, " ")
+end
+
+
+local function parse_refs(refs_str)
+  return utils.strsplit(refs_str, ",%s")
+end
+
+
 function M.parse(log)
   local data = {}
 
   local lines = utils.strsplit(log, "\n")
   for _, line in ipairs(lines) do
-    local line_data = utils.strsplit(line, "%z")
+    local hash, parents, refs, subject, author = unpack(utils.strsplit(line, "%z"))
 
     table.insert(
       data,
       {
-        hash = line_data[1],
-        parents = line_data[2],
-        refs = line_data[3],
-        subject = line_data[4],
-        author = line_data[5],
+        hash = hash,
+        parents = parse_parents(parents),
+        refs = parse_refs(refs),
+        subject = subject,
+        author = author,
       }
     )
   end
