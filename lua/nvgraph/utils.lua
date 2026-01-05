677bbcdfb464f7bca38afac73e06f9a2ea9bd0c3
Alexey Tsivunin <35929293+birthdaysgift@users.noreply.github.com>
2026-01-05 21:03:24 +0400

    Fix strsplit()

--- lua/nvgraph/utils.lua
+++ lua/nvgraph/utils.lua
@@ -1,14 +1,38 @@
 M = {}
 
 
-function M.strsplit(str, sep)
-    local t = {}
-    -- default separator is whitespace
-    sep = sep or "%s"
-    for field in str:gmatch("([^" .. sep .. "]+)") do
-        table.insert(t, field)
+function M.strsplit(s, sep, plain, n)
+    -- this function is just copied from https://github.com/lunarmodules/Penlight
+
+    local i1, ls = 1, {}
+    if not sep then
+        sep = '%s+'
     end
-    return t
+
+    if sep == '' then
+        return {s}
+    end
+
+    while true do
+        local i2, i3 = string.find(s, sep, i1, plain)
+        if not i2 then
+            local last = string.sub(s, i1)
+            if last ~= '' then
+                table.insert(ls, last)
+            end
+            if #ls == 1 and ls[1] == '' then
+                return {}
+            else
+                return ls
+            end
+        end
+        table.insert(ls, string.sub(s, i1, i2 - 1))
+        if n and #ls == n then
+            ls[#ls] = string.sub(s, i1)
+            return ls
+        end
+        i1 = i3 + 1
+    end
 end
 
 
